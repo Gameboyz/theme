9587e3f543280894991da37774824e8394644fad
Jared Smith <jared.smith.jrod@gmail.com>
2015-04-29 23:53:07 -0700

    basic tile layout, still need to get images working

--- content.php
+++ content.php
@@ -1,40 +1,29 @@
 <div class="gb-post clearfix">
-
-	<article id="post-<?php the_ID(); ?>" <?php post_class('gb-post-info'); ?>>
-
-		<h2 class="post-title">
+	<article id="post-<?php the_ID(); ?>" <?php post_class('gb-post-info');?> >
+		<h2 class="gb-post-text post-title" >
 			<a href="<?php esc_url(the_permalink()) ?>" rel="bookmark">
 				<?php the_title(); ?>
 			</a>
-
 		</h2>
 
-		<div class="entry clearfix">
+		<div class="gb-post-text entry clearfix" >
 			<?php the_excerpt(); ?>
 		</div>
 
-	</article>
-
-	<a href="<?php esc_url(the_permalink()) ?>" class="gb-img-link" >
 		<?php
-		$thumbnail = '';
+		$image = '';
 		$post_image_id = get_post_thumbnail_id($post->ID);
 
 		if ($post_image_id) {
-			$thumbnail = wp_get_attachment_image_src($post_image_id, 'post-thumbnail', false);
-			if ($thumbnail) (string)$thumbnail = $thumbnail[0];
+			$image = wp_get_attachment_image_src($post_image_id, 'post-large', false);
+			if ($image) (string)$image = $image[0];
 		}
 
-		if (!empty($thumbnail)) : ?>
-
-		<div class="gb-tile-img" style="background-image: url('<?php echo $thumbnail; ?>');" >
-
+		if (!empty($image)) : ?>
+			<div class="gb-tile-img" style="background-image: url('<?php echo $image; ?>');" >
 		<?php else: ?>
-
-		<div class="no-thumbnail">
-
+			<div class="no-thumbnail">
 		<?php endif; ?>
-
 		</div>
-	</a>
+	</article>
 </div>
