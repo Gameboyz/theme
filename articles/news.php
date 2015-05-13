f2d553e8f8ea0f55423f96b9a1866aa4038133de
Sam <samabev@gmail.com>
2015-05-12 17:57:39 -0700

    converted news.php to be different from review

--- articles/news.php
+++ articles/news.php
@@ -1,25 +1,23 @@
-<div id="gb-review-wrap">
+<div id="gb-news-wrap">
 
+      <?php
 
-
-
-      <?php 
         $image = '';
         $post_image_id = get_post_thumbnail_id($post->ID);
-        
+
         if($post_image_id) {
           $image = wp_get_attachment_image_src($post_image_id, 'post-large', false);
           if ($image) (string)$image = $image[0];
         }
-        
+
         if(!empty($image)) : ?>
-          <div class="gb-review-img" style="background-image: url('<?php echo $image; ?>');" >
+          <div class="gb-news-img" style="background-image: url('<?php echo $image; ?>');" >
         <?php endif; ?>
 
 
-      <section class="gb-review-header">
+      <section class="gb-news-header">
 
-        <h2 class="gb-review-title title"><?php the_title(); ?></h2>
+        <h2 class="gb-news-title title"><?php the_title(); ?></h2>
 
         <div class="postmeta"><?php momentous_display_postmeta(); ?></div>
 
@@ -36,10 +34,6 @@
   	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
 
       <div class="gb-game-info-wrap">
-
-        <?php the_title(); ?>
-
-          <div class="gb-game-info">(dev,pub,plat)</div>
 
       </div>
 
