6e52e7999ed0a9cf834addc9c9f65e4023070345
Jared Smith <jared.smith.jrod@gmail.com>
2015-06-01 13:33:49 -0700

    i should use the correct comment tags

--- articles/news.php
+++ articles/news.php
@@ -27,14 +27,14 @@
 	</section>
 
 
-	<?php // if ( function_exists('\Youneeq_Panel\recommend') ) : ?>
+	<?php /* if ( function_exists('\Youneeq_Panel\recommend') ) : */ ?>
 
 	<div id="youneeq-wrap-end">
 
-		<?php // \Youneeq_Panel\recommend(-2, ['title' => 'Also checkout:', 'count' => 4]); ?>
+		<?php /* \Youneeq_Panel\recommend(-2, ['title' => 'Also checkout:', 'count' => 4]); */ ?>
 
 	</div>
 
-	<?php endif; ?>
+	<?php /* endif; */ ?>
 
 </div>
